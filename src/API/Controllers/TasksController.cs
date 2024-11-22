83eb43daffcbb060de2561039d5f1c32a9ad0eaf
Gabriel Romario <gabriel.rcosta57@gmail.com>
2024-11-22 16:07:43 -0300

    add "api" to route

--- src/API/Controllers/TasksController.cs
+++ src/API/Controllers/TasksController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class TasksController(ITaskService taskService) : ControllerBase
 {
     [HttpPost]
